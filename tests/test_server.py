38e518790d94b3952bb7fc27953aa3922df62309
BeyzCoder <stevenbaes11@yahoo.com>
2024-01-30 17:42:14 -0600

    Fixed test case

--- tests/test_server.py
+++ tests/test_server.py
@@ -6,4 +6,4 @@ client = TestClient(app)
 def test_root():
     resp = client.get("/")
     assert resp.status_code == 200
-    assert resp.json() == {"Status" : "Server is up running!"}
+    assert resp.json() == {"Status" : "Server Running!"}
