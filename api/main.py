3ac48770cc4691d48aa169dbf2c5a335c62762a1
BeyzCoder <stevenbaes11@yahoo.com>
2024-01-30 18:24:24 -0600

    Added Mangum for lambda to control API

--- api/main.py
+++ api/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from mangum import Mangum
 
 app = FastAPI()
 
 @app.get("/")
 async def root() -> JSONResponse:
     return {"Status" : "Server Running!"}
+
+handler = Mangum(app=app)
